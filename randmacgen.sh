9e860e71e6655a47962379850753e7333ba6bce1
Aditya Vishwakarma <103015621+seanvishwakarma@users.noreply.github.com>
2022-06-22 20:45:44 +0530

    Update randmacgen.sh

--- randmacgen.sh
+++ randmacgen.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 macchanger -l > vendormac.txt
-ouimac=$(shuf -n vendor.txt | awk 'print$3')
+ouimac=$(shuf -n 1 vendormac.txt | awk '{print$3}')
 uaamac=$(printf '%02x:%02x:%02x' $[RANDOM%256] $[RANDOM%256] $[RANDOM%256])
 macchanger -m "$ouimac:$uaamac" eth0
